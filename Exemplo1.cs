2f176ad7385fb366f5066270061fd9c78f02ddf2
Kenneth <lordlqft0@gmail.com>
2026-03-24 09:49:44 -0300

    Exemplo1

---
+++ Exemplo1.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*
+
+*/
+
+
+namespace Aula_004
+{
+    public static class Exemplo1
+    {
+        public static void Executar()
+        {
+            double nota;
+            double frequencia;
+
+            Console.Write("Digite a nota do aluno: ");
+            nota = double.Parse(Console.ReadLine()!);
+
+            Console.Write("Digite a frequência do aluno: %");
+            frequencia = double.Parse(Console.ReadLine()!);
+
+            if (nota >= 7)
+            {
+                if (frequencia >= 75)
+                {
+                    Console.WriteLine("Aluno aprovado!");
+                }
+                else
+                {
+                    Console.WriteLine("Aluno reprovado por falta!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Aluno reprovado por nota!");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Da pra parar de olhar meu perfil?");
+﻿using System;
+using Aula_004;
+
+Exemplo1.Executar();
